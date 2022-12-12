efaa5fc1df375d32d0e038fbaefba1a72b0cd03d
jptak04 <ptakjakub004@gmail.com>
2022-12-12 19:12:59 +0100

    Del nested ifs

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -11,29 +11,23 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (items[i].quality > 0)
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        
+        if ((items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")&&((items[i].quality > 0)&&(items[i].name != "Sulfuras, Hand of Ragnaros"))) 
                     items[i].quality--;
-        }
         else if (items[i].quality < 50)
             {
                 items[i].quality++;
 
                 if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (items[i].sellIn < 11)
-                        if (items[i].quality < 50)
+                    if ((items[i].sellIn < 11)&&(items[i].quality < 50))
                             items[i].quality++;
 
-                    if (items[i].sellIn < 6)
-                        if (items[i].quality < 50)
+                    if ((items[i].sellIn < 6)&&(items[i].quality < 50))
                             items[i].quality++;
                 }
             }
-
-
+            
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
             items[i].sellIn --;
 
@@ -43,16 +37,14 @@ void GildedRose::updateQuality()
             {
                 if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (items[i].quality > 0)
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                    if ((items[i].quality > 0)and(items[i].name != "Sulfuras, Hand of Ragnaros"))
                             items[i].quality --;
                 }
                 else
                     items[i].quality --;
             }
             else if (items[i].quality < 50)
-                items[i].quality++;
-                
+                items[i].quality++;  
         }
     }
 }
