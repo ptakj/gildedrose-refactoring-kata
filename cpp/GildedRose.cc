48fba444527409b67bd8e79c5aa81882f8e01d58
jptak04 <ptakjakub004@gmail.com>
2022-12-12 19:15:44 +0100

    Visual changes

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,17 +1,12 @@
 #include "GildedRose.h"
-
-
 GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 {}
-
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
-
 void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        
         if ((items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")&&((items[i].quality > 0)&&(items[i].name != "Sulfuras, Hand of Ragnaros"))) 
                     items[i].quality--;
         else if (items[i].quality < 50)
